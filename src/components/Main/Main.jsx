03b67e5a7a4879a4020f218d8f7cb7d30e253e7a
Darpeex <foxyyysp@mail.ru>
2023-10-11 07:53:29 +0300

    refactor: добавлены картинки, стили, main стал чище

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,12 @@
 // Основное содержимое страницы
 import React from 'react';
 import { Movie } from '../Movies/Movies';
-import { CurrentUserContext } from '../../context/CurrentUserContext';
 import { MoviesContext } from '../../context/MoviesContext';
+import { CurrentUserContext } from '../../context/CurrentUserContext';
 
 export function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick, onCardLike, onCardDelete }) {  // Передаются функции открытия попапов из App.js
-  const currentUser = React.useContext(CurrentUserContext); // Подписываемся на контекст пользователя, а далее в html передаём данные в соответствующие поля
-  const cards = React.useContext(MoviesContext); // Подписываемся на контекст карточек, затем передаём массив карточек и обрабатываем их
+  const currentUser = React.useContext(CurrentUserContext); // Подписываемся на контекст пользователя, далее в html передаём данные в соответствующие поля
+  const movies = React.useContext(MoviesContext); // Подписываемся на контекст фильмов, затем передаём массив карточек и обрабатываем их
   return (
     <main className="content">
 
@@ -28,7 +28,7 @@ export function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick, onC
 
       {/* Отрисовываем каждую карточку при помощи компонента Card и возвращаем в разметку внутрь section */}
       <section id="elements" className="elements page__elements-position section">
-        {cards.map(card => (// Пробегаем по переданному массиву и возвращаем целые карточки при помощи разметки
+        {movies.map(card => (// Пробегаем по переданному массиву и возвращаем целые карточки при помощи разметки
           <Movie key={card._id} card={card} onCardClick={onCardClick} onCardLike={onCardLike} onCardDelete={onCardDelete} />))}
       </section>
     </main>
