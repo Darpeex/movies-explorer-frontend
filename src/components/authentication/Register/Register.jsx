e23788291cb0a9c8f8b4ed743aa2429f2ed05048
Darpeex <foxyyysp@mail.ru>
2023-10-17 15:57:13 +0300

    feat: свёрстан компонент Register

--- src/components/authentication/Register/Register.jsx
+++ src/components/authentication/Register/Register.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import * as auth from '../../../utils/Auth';
 
 export const Register = ({ onInfoTooltip, onResult, errorMessage }) => {
+  const [isError, setIsError] = useState(false)
   const [formValue, setFormValue] = useState({
     password: '',
     email: '',
@@ -27,7 +28,7 @@ export const Register = ({ onInfoTooltip, onResult, errorMessage }) => {
     const { password, email } = formValue;
     auth.register(password, email).then((res) => {
       onResult(true)
-      navigate('/sign-in', { replace: true });
+      navigate('/signin', { replace: true });
     }).catch((err) => {
       onResult(false)
       errorMessage('Email зарегистрирован или данные неверны')
@@ -37,17 +38,55 @@ export const Register = ({ onInfoTooltip, onResult, errorMessage }) => {
 
   return (
     <div className="register">
-      <p className="register__welcome">Добро пожаловать!</p>
+      <h1 className="register__welcome">Добро пожаловать!</h1>
       <form onSubmit={handleSubmit} className="register__form">
-        <input required minLength="2" maxLength="30" id="email" name="email" type="email" value={formValue.email} onChange={handleChange} placeholder="Email" />
-        <input required minLength="8" maxLength="30" id="password" name="password" type="password" value={formValue.password} onChange={handleChange} placeholder="Пароль" />
+        <span className="register__form_field_name">Имя</span>
+        <input
+          className="register__form-input"
+          minLength="2"
+          maxLength="30"
+          id="name"
+          name="name"
+          type="name"
+          value={formValue.name}
+          onChange={handleChange}
+          placeholder="Введите имя"
+          required
+        />
+        <span className="register__form_field_name">E-mail</span>
+        <input
+          className="register__form-input"
+          minLength="8"
+          maxLength="30"
+          id="email"
+          name="email"
+          type="email"
+          value={formValue.email}
+          onChange={handleChange}
+          placeholder="Введите e-mail"
+          required
+        />
+        <span className="register__form_field_name">Пароль</span>
+        <input
+          className={`register__form-input ${isError ? "register__form_field_margin_off" : "" }`}
+          minLength="8"
+          maxLength="30"
+          id="password"
+          name="password"
+          type="password"
+          value={formValue.password}
+          onChange={handleChange}
+          placeholder="Введите пароль"
+          required
+        />
+        {isError && <span className="register__form_error">Что-то пошло не так...</span>}
         <div className="register__button-container">
-          <button type="submit" className="register__link">Зарегистрироваться</button>
+          <button type="submit" className="register__button">Зарегистрироваться</button>
         </div>
       </form>
       <div className="register__signup">
-        <p>Уже зарегистрированны?</p>
-        <Link to="/sign-in" className="signup__link">Войти</Link>
+        <p className="register__signup_text">Уже зарегистрированны?</p>
+        <Link to="/signin" className="signup__button">Войти</Link>
       </div>
     </div>
   )
