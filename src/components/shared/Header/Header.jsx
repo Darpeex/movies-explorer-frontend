0c27109d9440833c3d49aad5706fa0423e345308
Darpeex <foxyyysp@mail.ru>
2023-10-19 14:59:54 +0300

    perf: изменения Header в соответствии с медиа-запросом

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,13 +1,28 @@
 // Шапка сайта
 import './Header.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BurgerMenu } from './BurgerMenu/BurgerMenu';
 import { ROUTES } from '../../../constants/constants.js';
 import { Routes, Route, NavLink, Link } from 'react-router-dom';
+
 const { HOME, MOVIES, SAVED_MOVIES, PROFILE, SIGNUP, SIGNIN } = ROUTES;
 const whiteBackgroundPaths = [MOVIES, SAVED_MOVIES, PROFILE, SIGNIN, SIGNUP];
 const headerOnlyHomeLogo = [SIGNIN, SIGNUP];
 const linkLogoHome = <Link className="header__logo" to={HOME} aria-label="Главная"></Link>
+
+const useMediaQuery = (query) => { // query - медиа-запрос, который совместим с CSS медиа-запросом
+  const [matches, setMatches] = useState(() => window.matchMedia(query).matches); // инициализируется возвращенным значением `() => window.matchMedia(query).matches` - matches будет true, если ширина экрана = переданному медиа-запросу (query)
+  useEffect(() => { // при каждом вызове этого эффектa...
+    const media = window.matchMedia(query); // ...создается новый объект являющийся переданным медиа-запросом
+    const listener = (event) => { // listener вызывается, когда изменяется медиа-запрос
+      setMatches(event.matches); // и обновляет состояние matches
+    };
+
+    media.addEventListener('change', listener); // Добавляем обработчик событий, который вызывает listener при каждом изменении состояния медиа-запроса
+    return () => media.removeEventListener('change', listener); // удаляем слушателя при размонтировании компонента, чтобы избежать утечек памяти
+  }, [query]); // useEffect вызывается, только когда query - состояние медиа-запроса изменияется
+  return matches; // возвращается состояние, по которому и определяется соответствие текущего состояние устройства указанному медиа-запросу
+};
 
 const Burger = ({ location, accountButton }) => {
   const items = [
@@ -72,18 +87,31 @@ export function Header({ location, loggedIn }) {
     </Link>
   )
 
-  const headerNav = <HeaderNavigation accountButton={accountButton} headerBackgroundWhiteClass={headerBackgroundWhiteClass} isActiveLink={isActiveLink} />;
+  const headerNav = <HeaderNavigation accountButton={accountButton} isActiveLink={isActiveLink} />;
+  const burgerNav = <Burger accountButton={accountButton} location={location} />;
+
+  const isMobile = useMediaQuery('(max-width: 768px)');
 
   return (
     <header className={`header header_position ${headerBackgroundWhiteClass} ${headerOnlyHomeLogoClass}`}>
       <Routes>
-        <Route path={HOME} element={loggedIn ? headerNav : <Unauthorized />} />
-        <Route path={MOVIES} element={headerNav} />
-        <Route path={SAVED_MOVIES} element={headerNav} />
-        <Route path={PROFILE} element={headerNav} />
-        <Route path={SIGNIN} element={linkLogoHome} />
-        <Route path={SIGNUP} element={linkLogoHome} />
-        <Route path={'/burger'} element={<Burger accountButton={accountButton} location={location} />} /> {/* test string */}
+        {isMobile ? (
+          <>
+            <Route path={HOME} element={loggedIn ? burgerNav : <Unauthorized />} />
+            <Route path={MOVIES} element={burgerNav} />
+            <Route path={SAVED_MOVIES} element={burgerNav} />
+            <Route path={PROFILE} element={burgerNav} />
+          </>
+        ) : ( // Если @media screen and (max-width: 768px)
+          <>
+            <Route path={HOME} element={loggedIn ? headerNav : <Unauthorized />} />
+            <Route path={MOVIES} element={headerNav} />
+            <Route path={SAVED_MOVIES} element={headerNav} />
+            <Route path={PROFILE} element={headerNav} />
+            <Route path={SIGNIN} element={linkLogoHome} />
+            <Route path={SIGNUP} element={linkLogoHome} />
+          </>
+        )}
       </Routes>
     </header>
   );
