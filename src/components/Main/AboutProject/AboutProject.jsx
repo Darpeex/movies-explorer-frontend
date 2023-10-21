286b371c366c2352643161bb1a87297777fac476
Darpeex <foxyyysp@mail.ru>
2023-10-21 22:38:48 +0300

    fix: HOME исправлен по BEMValidator

--- src/components/Main/AboutProject/AboutProject.jsx
+++ src/components/Main/AboutProject/AboutProject.jsx
@@ -5,12 +5,12 @@ export function AboutProject({scrollToRef}) {
 	return (	// Секция AboutProject
 		<section className="about-project about-project_position section" ref={scrollToRef}>
 			<h2 className="about-project__title">О проекте</h2>
-			<div class="about-project__line line"></div>
+			<div className="about-project__line line"></div>
 			<div className="about-project__description">
-				<h3 className="about-project__description_title">Дипломный проект включал 5 этапов</h3>
-				<h3 className="about-project__description_title">На&nbsp;выполнение диплома ушло 5 недель</h3>
-				<p className="about-project__description_info">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и&nbsp;финальные доработки.</p>
-				<p className="about-project__description_info">У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+				<h3 className="about-project__definition about-project__definition_title">Дипломный проект включал 5 этапов</h3>
+				<h3 className="about-project__definition about-project__definition_title">На&nbsp;выполнение диплома ушло 5 недель</h3>
+				<p className="about-project__definition about-project__definition_info">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и&nbsp;финальные доработки.</p>
+				<p className="about-project__definition about-project__definition_info">У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
 			</div>
 			<div className="about-project__table-grid">
 				<p className="about-project__timeline about-project__timeline_green">1 неделя</p>
