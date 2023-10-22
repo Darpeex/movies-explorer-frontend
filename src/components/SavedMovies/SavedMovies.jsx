e3030c3aaba5e82f88567f2d1b82831e54a53456
Darpeex <foxyyysp@mail.ru>
2023-10-22 10:39:55 +0300

    fix: SAVED_MOVIES исправлен по BEMValidator

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -13,8 +13,8 @@ export function SavedMovies() {  // Передаются функции из App
       <SearchForm />
       {/* <MoviesCardList />  Отрисовывать только карточки, на которых стоит лайк */}
       {moreMovies && <button className="content__button-more" type="button">Ещё</button>} {/* Показывать, если ещё остались фильмы, иначе не отрисовывать элемент */}
-      {!moreMovies && <div className={`content__additional-block ${!movieFound ? "content__additional-block_margin_zero" : ""}`}>
-        {!movieFound && <p className="content__additional-block_result_empty">Фильм не найден</p>} {/* Если фильм не найден - покажется содержимое этого блока */}
+      {!moreMovies && <div className={`content__block-additional ${!movieFound ? "content__block-additional_margin_zero" : ""}`}>
+        {!movieFound && <p className="content__additional-block-result content__additional-block-result_empty">Фильм не найден</p>} {/* Если фильм не найден - покажется содержимое этого блока */}
       </div>}
     </main>
   )
