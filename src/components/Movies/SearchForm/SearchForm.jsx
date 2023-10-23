89afd8042dab49b29e9a2be1fe0dbfc8f20cd6d0
Darpeex <foxyyysp@mail.ru>
2023-10-23 17:24:06 +0300

    perf: улучшен блок поиска по фильмам - SearchForm

--- src/components/Movies/SearchForm/SearchForm.jsx
+++ src/components/Movies/SearchForm/SearchForm.jsx
@@ -1,23 +1,48 @@
 // Форма поиска фильмов
-import React from 'react';
 import './SearchForm.css';
+import React, { useEffect } from 'react';
 import { FilterChecbox } from '../FilterCheckbox/FilterCheckbox';
 
 export function SearchForm() {
-  const [isInputFocused, setInputFocus] = React.useState(false);
+  const [isInputFocused, setInputFocus] = React.useState(false); // для подчеркивания input при фокусе
+  const [isEmpty, setIsEmpty] = React.useState(false); // состояние введенной информации
+  const [value, setValue] = React.useState(""); // состояние введенной информации
+  
+  useEffect(() => { // нужно проверить поле после вывода ошибки
+    if (value !== "") { // чтобы после ввода текста сообщение убиралось*
+      setIsEmpty(false)
+    }
+  }, [value]);
+
+  function handleValueChange(e) { // по событию на элементе устанавливаем значение value
+    setValue(e.target.value); // чтобы взять значение из поля value*
+  }
+
+  function checkInputField(e) { // проверяем пустое ли поле по клику
+    e.preventDefault();
+    if (value === "") { // чтобы проверить поле перед отправкой ЛКМ*
+      setIsEmpty(true);
+    } else {
+      setIsEmpty(false);
+    }
+  }
 
   return (
     <section className="search-form search-form_position section">
-      <div className="search-form__container">
+      <form className="search-form__container" onSubmit={checkInputField}>
         <input
           className="search-form__input"
           placeholder="Фильм"
+          type="text"
+          value={value}
           onFocus={() => setInputFocus(true)}
           onBlur={() => setInputFocus(false)}
+          onChange={handleValueChange}
         />
-        <button className="search-form__button" type="button">Найти</button>
-      </div>
-      <div className={`search-form__underline ${isInputFocused ? "search-form__underline-focused" : ""}`}></div>
+        <button className="search-form__button" type="submit">Найти</button>
+      </form>
+      <div className={`search-form__underline ${isInputFocused ? "search-form__underline_focused" : ""} ${isEmpty ? "search-form__underline_error" : ""}`}></div>
+      {isEmpty && <span className="search-form__messsage search-form__messsage_error">Нужно ввести ключевое слово</span>}
       <div className="search-form__wrapper">
         <FilterChecbox />
         <span className="search-form__short-film">Короткометражки</span>
