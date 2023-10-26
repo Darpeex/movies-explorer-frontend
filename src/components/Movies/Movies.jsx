cc857600979534109a653c0d0380c6e7fd9902c5
Darpeex <foxyyysp@mail.ru>
2023-10-26 21:24:41 +0300

    feat: поиск по фильмам работает более корректно

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,18 +1,26 @@
 // Раздел с фильмами
 import './Movies.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { SearchForm } from './SearchForm/SearchForm';
+import { MoviesContext } from '../../context/MoviesContext';
 import { MoviesCardList } from './MoviesCardList/MoviesCardList';
 
 export function Movies({ loadMovies }) {  // Передаются функции из App.js
   const [moreMovies, setMoreMovies] = useState(false);
   const [movieFound, setMovieFound] = useState(true);
-  const [searchResults, setSearchResults] = useState([]);
+  const [initialMovies, setInitialMovies] = useState([]);
+  const { movies, setMovies } = useContext(MoviesContext); // исходный список найденых по запросу
+
+  console.log(initialMovies)
+  // сохраняем исходный список фильмов при получении movies
+  useEffect(() => {
+    setInitialMovies(movies);
+  }, [movies]);
 
   return (
     <main className="content">
-      <SearchForm loadMovies={loadMovies} setSearchResults={setSearchResults} />
-      <MoviesCardList searchResults={searchResults} />
+      <SearchForm loadMovies={loadMovies} initialMovies={initialMovies} setInitialMovies={setInitialMovies} />
+      <MoviesCardList initialMovies={initialMovies} />
       {moreMovies && <button className="content__button-more" type="button">Ещё</button>}
       {!moreMovies && <div className={`content__additional-block ${!movieFound ? "content__additional-block_margin_zero" : ""}`}>
         {!movieFound && <p className="content__additional-block_result_empty">Ничего не найдено</p>}
--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -6,7 +6,7 @@ import { MoviesCard } from '../MoviesCard/MoviesCard';
 export function MoviesCardList({ onCardLike, onCardDelete, initialMovies }) {  // Передаются функции открытия попапов из App.js
   return ( // Отрисовываем каждый фильм при помощи компонента Movie и возвращаем в разметку внутрь section
     <section id="elements" className="elements page__elements-position section">
-      {initialMovies.map(movie => (// Пробегаем по переданному массиву и возвращаем карточки с фильмами
+      {initialMovies && initialMovies.map(movie => (// Пробегаем по переданному массиву и возвращаем карточки с фильмами
         <MoviesCard key={movie.id} movie={movie} onCardLike={onCardLike} onCardDelete={onCardDelete} />))}
     </section>
   )
