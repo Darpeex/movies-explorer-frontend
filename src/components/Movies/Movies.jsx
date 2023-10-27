2e815e471aa510691553a42553f2260667ab9327
Darpeex <foxyyysp@mail.ru>
2023-10-27 16:30:07 +0300

    feat: скрываем блок с фильмами, если они не найдены

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -13,7 +13,7 @@ export function Movies({ loadMovies }) {  // Передаются функции
   return (
     <main className="content">
       <SearchForm loadMovies={loadMovies} initialMovies={initialMovies} setInitialMovies={setInitialMovies} setFilteredMovies={setFilteredMovies} setMovieFound={setMovieFound} />
-      <MoviesCardList initialMovies={initialMovies} filteredMovies={filteredMovies} />
+      {movieFound &&<MoviesCardList initialMovies={initialMovies} filteredMovies={filteredMovies} />}
       {moreMovies && <button className="content__button-more" type="button">Ещё</button>}
       {!moreMovies && <div className={`content__additional-block ${!movieFound ? "content__additional-block_margin_zero" : ""}`}>
         {!movieFound && <p className="content__additional-block_result_empty">Ничего не найдено</p>}
--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -2,16 +2,17 @@
 import './SavedMovies.css';
 import React, { useState } from 'react';
 import { SearchForm } from '../Movies/SearchForm/SearchForm';
-import { MoviesCardList } from '../Movies/MoviesCardList/MoviesCardList';
 
-export function SavedMovies({ loadingFilms }) {  // Передаются функции из App.js
+export function SavedMovies() {  // Передаются функции из App.js
   const [moreMovies, setMoreMovies] = useState(false);
-  const [movieFound, setMovieFound] = useState(false);
+  const [movieFound, setMovieFound] = useState(true);
+  const [initialMovies, setInitialMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
 
   return (
     <main className="content">
-      <SearchForm loadingFilms={loadingFilms} />
-      {/* <MoviesCardList />  Отрисовывать только карточки, на которых стоит лайк */}
+      <SearchForm initialMovies={initialMovies} setInitialMovies={setInitialMovies} setFilteredMovies={setFilteredMovies} setMovieFound={setMovieFound} />
+      {/* {movieFound && <MoviesCardList />}  Отрисовывать только карточки, на которых стоит лайк */}
       {moreMovies && <button className="content__button-more" type="button">Ещё</button>} {/* Показывать, если ещё остались фильмы, иначе не отрисовывать элемент */}
       {!moreMovies && <div className={`content__block-additional ${!movieFound ? "content__block-additional_margin_zero" : ""}`}>
         {!movieFound && <p className="content__additional-block-result content__additional-block-result_empty">Фильм не найден</p>} {/* Если фильм не найден - покажется содержимое этого блока */}
