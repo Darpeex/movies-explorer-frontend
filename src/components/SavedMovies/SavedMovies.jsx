643587fd8b6d6f3829d21bf49d272ea6593e3d64
Darpeex <foxyyysp@mail.ru>
2023-10-29 18:12:02 +0300

    refactor: обновлен компонент SavedMovies

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -5,19 +5,28 @@ import { SearchForm } from '../Movies/SearchForm/SearchForm';
 import { MoviesCardList } from '../SavedMovies/MoviesCardList/MoviesCardList';
 import { SavedMoviesContext } from '../../context/SavedMoviesContext';
 
-export function SavedMovies({ loadMovies, loadingError, handleDeleteClick }) {  // Передаются функции из App.js
-  const [moreMovies, setMoreMovies] = useState(false);
-  const [movieFound, setMovieFound] = useState(true);
-  const [initialMovies, setInitialMovies] = useState([]);
-  const [filteredMovies, setFilteredMovies] = useState([]);
+export function SavedMovies({ loadingError, handleDeleteClick }) {
   const savedMovies = React.useContext(SavedMoviesContext);
+  const [movieFound, setMovieFound] = useState(true); // найдены ли фильмы по запросу?
+  const [initialMovies, setInitialMovies] = useState(savedMovies); // все найденые фильмы по запросу
+  const [filteredMovies, setFilteredMovies] = useState([]); // отфильтрованное чекбоксом
+  const [moviesToShow, setMoviesToShow] = useState([]); // фильмы, которые должны отрисоваться
+  const [onSubmit, setOnSubmit] = useState(false); // отслеживаем вызов submit поиска
+  const [isChecked, setIsChecked] = useState(false); // нажат чексбокс или нет
+
+  const errorConditions = loadingError || (loadingError !== null) || !movieFound; // условия, при которых не показываются другие блоки
+  const errorСonditionsMovies = movieFound && !errorConditions && (savedMovies.length !== 0); // условия, при которых показывается блок с фильмами
+  const errorСonditionsAdittionalBlock = !movieFound || !errorConditions; // условия, при которых показывается дополнительный блок
 
   return (
     <main className="content">
-      <SearchForm initialMovies={initialMovies} setInitialMovies={setInitialMovies} setFilteredMovies={setFilteredMovies} setMovieFound={setMovieFound} />
-      {movieFound && <MoviesCardList savedMovies={savedMovies} handleDeleteClick={handleDeleteClick} />}  Отрисовывать только карточки, на которых стоит лайк
-      {!moreMovies && <div className={`content__block-additional ${!movieFound ? "content__block-additional_margin_zero" : ""}`}>
-        {!movieFound && <p className="content__additional-block-result content__additional-block-result_empty">Ничего не найдено</p>} {/* Если фильм не найден - покажется содержимое этого блока */}
+      <SearchForm initialMovies={initialMovies} setInitialMovies={setInitialMovies} setFilteredMovies={setFilteredMovies} setMovieFound={setMovieFound} setIsChecked={setIsChecked} />
+      {errorСonditionsMovies && <MoviesCardList savedMovies={savedMovies} handleDeleteClick={handleDeleteClick} />}
+      {errorСonditionsAdittionalBlock && <div className="content__additional-block">
+        {!movieFound && <p className="content__additional-block_result_empty">Ничего не найдено</p>}
+      </div>}
+      {loadingError && <div className="content__error-additional-block">
+        <p className="content__error-additional-block_result_error">{loadingError}</p>
       </div>}
     </main>
   )
