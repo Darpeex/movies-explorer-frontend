06e3da2ba3c2ab15d36e949ab8d6f9fd9c3caa16
Darpeex <foxyyysp@mail.ru>
2023-10-30 12:46:14 +0300

    perf: улучшена форма регистрации через Formik

--- src/components/authentication/Register/Register.jsx
+++ src/components/authentication/Register/Register.jsx
@@ -4,95 +4,90 @@ import React, { useState } from 'react';
 import * as auth from '../../../utils/Auth';
 import { Link, useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../../constants/constants';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-export const Register = ({ onInfoTooltip, onResult, errorMessage }) => {
-  const [isError, setIsError] = useState(false)
-  const [formValue, setFormValue] = useState({
-    name: '',
-    email: '',
-    password: '',
-  })
+export const Register = ({ onInfoTooltip, onResult, error, setError }) => {
+  const [isFormValid, setIsFormValid] = useState(false);
   const navigate = useNavigate();
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValue({
-      ...formValue,
-      [name]: value,
-    });
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const { name, email, password } = formValue;
-    auth.register(name, password, email).then((res) => {
-      onResult(true)
-      navigate('/signin', { replace: true });
-    }).catch((err) => {
-      onResult(false)
-      errorMessage('Email зарегистрирован или данные неверны')
-    })
-      .finally(onInfoTooltip)
-  }
 
   return (
     <main>
       <section className="register">
         <h1 className="register__welcome">Добро пожаловать!</h1>
-        <form onSubmit={handleSubmit} className="register__form">
-          <label className="register__label">Имя</label>
-          <input
-            className="register__form-input"
-            minLength="2"
-            maxLength="30"
-            id="name"
-            name="name"
-            type="text"
-            value={formValue.name}
-            onChange={handleChange}
-            placeholder="Ваше имя"
-            required
-          />
-          <label className="register__label">E-mail</label>
-          <input
-            className="register__form-input"
-            minLength="8"
-            maxLength="30"
-            id="email"
-            name="email"
-            type="email"
-            value={formValue.email}
-            onChange={handleChange}
-            placeholder="example@email.ru"
-            required
-          />
-          <label className="register__label">Пароль</label>
-          <input
-            className={`register__form-input ${isError ? "register__form-input_error" : ""}`}
-            minLength="8"
-            maxLength="30"
-            id="password"
-            name="password"
-            type="password"
-            value={formValue.password}
-            onChange={handleChange}
-            placeholder="Введите пароль"
-            required
-          />
-          {isError && <span className="register__form-error">Что-то пошло не так...</span>}
-          <div className="register__button-container">
-            <button type="submit" className="register__button">Зарегистрироваться</button>
-          </div>
-        </form>
-        <div className="register__signup">
-          <p className="register__text-signup">Уже зарегистрированы?</p>
-          <Link to={ROUTES.SIGNIN} className="register__signin-button">Войти</Link>
-        </div>
+        <Formik
+          initialValues={{ name: '', email: '', password: '' }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.name) {
+              errors.name = 'Пожалуйста, введите имя';
+            } else if (!/^[a-zA-Zа-яА-Я\s-]+$/.test(values.name)) {
+              errors.name = "Допустимые символы: латиница, кириллица, пробел и дефис";
+            } else if (!(2 <= values.name.length)) {
+              errors.name = 'Имя не может быть менее 2 символов';
+            } else if ((values.name.length >= 30)) {
+              errors.name = 'Имя не может быть более 30 символов';
+            }
+            if (!values.email) {
+              errors.email = 'Пожалуйста, введите e-mail';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+              errors.email = 'Некорректный e-mail адрес';
+            }
+            if (!values.password) {
+              errors.password = 'Пожалуйста, введите пароль';
+            } else if (!(8 <= values.password.length)) {
+              errors.password = 'Длина пароля не меньше 8 символов';
+            }
+            setIsFormValid(Object.keys(errors).length === 0);
+            setError('')
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            auth.register(values.name, values.password, values.email)
+              .then((res) => {
+                onResult(true)
+                navigate('/movies', { replace: true });
+              })
+              .catch((err) => {
+                onResult(false)
+                setError('Email зарегистрирован или данные неверны')
+              })
+              .finally(() => {
+                onInfoTooltip();
+              })
+          }}
+        >
+          {({ isSubmitting, errors, touched }) => (
+            <Form className="register__form" noValidate> {/* noValidate - отключена браузерная валидация */}
+              <label htmlFor="name" className="register__label">Имя</label>
+              <Field type="text" name="name" className={`register__form-input ${errors.name && touched.name ? "register__form-input_error" : ""}`} placeholder="Введите имя" />
+
+              <ErrorMessage name="name" component="div" className="register__form-error" />
+
+              <label htmlFor="email" className="register__label">E-mail</label>
+              <Field type="email" name="email" className={`register__form-input ${errors.email && touched.email ? "register__form-input_error" : ""}`} placeholder="example@email.ru" />
+
+              <ErrorMessage name="email" component="div" className="register__form-error" />
+
+              <label htmlFor="password" className="register__label">Пароль</label>
+              <Field type="password" name="password" className={`register__form-input ${errors.password && touched.password ? "register__form-input_error" : ""}`} placeholder="Введите пароль" />
+
+              <ErrorMessage name="password" component="div" className="register__form-error" />
+
+              <div className="register__button-container">
+                {(error !== '') && <p className="register__error-message">{error}</p>}
+                <button type="submit" className="register__button" disabled={isSubmitting || !isFormValid}>
+                  Зарегистрироваться
+                </button>
+              </div>
+
+              <div className="register__signup">
+                <p className="register__text-signup">Уже зарегистрированы?</p>
+                <Link to={ROUTES.SIGNIN} className="register__signin-button">Войти</Link>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </section>
     </main>
   )
 }
-
-// Ошибки ниже отображаются не под инпутами, а при получении ошибки после отправки запроса
-// Пользователь с&nbsp;таким email уже существует.
-// При регистрации пользователя произошла ошибка.
