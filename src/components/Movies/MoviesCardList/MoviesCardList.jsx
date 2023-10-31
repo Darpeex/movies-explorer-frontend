0f9e76ab3bcf77ebad79f15622c3f50d2565138b
Darpeex <foxyyysp@mail.ru>
2023-10-31 19:18:05 +0300

    refactor: компоненты почищены onCardLike

--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -2,11 +2,11 @@
 import './MoviesCardList.css';
 import { MoviesCard } from '../MoviesCard/MoviesCard';
 
-export function MoviesCardList({ onCardLike, moviesToShow, handleLikeClick }) {
+export function MoviesCardList({ moviesToShow, handleLikeClick }) {
   return ( // Отрисовываем каждый фильм при помощи компонента Movie и возвращаем в разметку внутрь section
     <section id="elements" className="elements page__elements-position section">
       {moviesToShow && moviesToShow.map(movie => ( // Пробегаем по переданному массиву и возвращаем карточки с фильмами
-        <MoviesCard key={movie.id} movie={movie} onCardLike={onCardLike} handleLikeClick={handleLikeClick} />))}
+        <MoviesCard key={movie.id} movie={movie} handleLikeClick={handleLikeClick} />))}
     </section>
   )
 }
--- src/components/SavedMovies/SavedMoviesCardList/SavedMoviesCardList.jsx
+++ src/components/SavedMovies/SavedMoviesCardList/SavedMoviesCardList.jsx
@@ -1,9 +1,8 @@
 // Раздел для карточек с сохранёнными фильмами
 import './SavedMoviesCardList.css';
-import React from 'react';
 import { SavedMoviesCard } from '../SavedMoviesCard/SavedMoviesCard';
 
-export function SavedMoviesCardList({ onCardLike, savedMovies, handleDeleteClick }) {
+export function SavedMoviesCardList({ savedMovies, handleDeleteClick }) {
   return ( // Отрисовываем каждый фильм при помощи компонента Movie и возвращаем в разметку внутрь section
     <section id="elements" className="elements page__elements-position section">
       {savedMovies && savedMovies.map(movie => ( // Пробегаем по переданному массиву и возвращаем карточки с фильмами
